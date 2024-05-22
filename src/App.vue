58d572a1d17a6a80c5c7ad62af167c2f5887cbf0
Artemii29 <artemii.kharitonov@bk.ru>
2024-05-22 21:10:18 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <carousel :items="Items" />
+</template>
+
+<script>
+import Carousel from './components/Carousel.vue';
+
+export default {
+  components: {
+    Carousel
+  },
+  data() {
+    return {
+      Items: [
+        { type: 'text', content: 'Пример текста', background: 'grey' },
+        { type: 'image', src: 'image.jpg', background: 'green' },
+        { type: 'input', placeholder: 'Введите текст', background: 'purple' },
+        { type: 'video', src: 'video.mp4', background: 'orangered' }
+      ]
+    };
+  }
+};
+</script>
---
+++ src/components/Carousel.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="carousel">
+    <div v-for="(item, index) in items" :key="index" class="carousel-item" :style="{ background: item.background }">
+      <p v-if="item.type === 'text'">{{ item.content }}</p>
+
+      <img v-else-if="item.type === 'image'" :src="item.src">
+
+      <input v-else-if="item.type === 'input'" type="text" :placeholder="item.placeholder">
+
+      <video v-else-if="item.type === 'video'" :src="item.src" controls>
+        <source >
+      </video>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['items']
+};
+</script>
+
+<style>
+.carousel {
+  aspect-ratio: 2 / 1;
+  display: flex;
+  overflow-x: auto;
+}
+
+.carousel-item {
+  flex: 0 0 auto;
+  width: 100%;
+  height: 100%;
+}
+</style>
